d47872e1518ada303405eef9d69fbc37c6e32b97
srbiz <sidneibiz@gmail.com>
2017-04-06 00:16:22 -0300

    new forms

--- src/app/components/form/Form.js
+++ src/app/components/form/Form.js
@@ -1,14 +1,37 @@
 class FormController {
   constructor() {
-    this.teste = 'TESTE';
+    this.inputs = [{
+      type: 'email',
+      id: 1,
+      label: 'teste',
+      disabled: 'disabled',
+      sucess: 'Sensacional',
+      error: 'Porra Jamelão ve se cadastra certo essa porrra',
+      class: 'validate'
+    },
+    {
+      type: 'email',
+      id: 2,
+      label: 'teste2',
+      disabled: false,
+      error: 'Ex:exemplo@email.com',
+      value: 'sidnei@gmail.com',
+      class: 'validate'
+    }];
   }
 }
 
-export const Footer = {
+export const Form = {
   template: require('./Form.html'),
   controller: FormController,
   bindings: {
     description: '@',
-    icon: '@'
+    icon: '@',
+    type: '@',
+    value: '@',
+    disabled: '=',
+    sucess: '@',
+    error: '@',
+    class: '@'
   }
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import angular from 'angular';
 import 'mdbootstrap/js/bootstrap.js';
 import 'mdbootstrap/js/mdb.js';
 
-// import {TodoService} from './app/todos/todos';
-// import {App} from './app/containers/App';
-// import {Header} from './app/components/Header';
-// import {MainSection} from './app/components/MainSection';
-// import {TodoTextInput} from './app/components/TodoTextInput';
-// import {TodoItem} from './app/components/TodoItem';
 import {Form} from './app/components/form/Form';
 import 'angular-ui-router';
 import routesConfig from './routes';
@@ -21,10 +15,4 @@ import './index.scss';
 angular
   .module('app', ['ui.router'])
   .config(routesConfig)
-  // .service('todoService', TodoService)
-  // .component('app', App);
-  // .component('headerComponent', Header)
   .component('formComponent', Form);
-  // .component('mainSection', MainSection)
-  // .component('todoTextInput', TodoTextInput)
-  // .component('todoItem', TodoItem);
